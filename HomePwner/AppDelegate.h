f22d745f49604d2b092bee0daf726d38745ac860
cocos543 <543314602@qq.com>
2015-11-24 08:44:21 +0800

    Initial Commit

---
+++ HomePwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomePwner
+//
+//  Created by 郑克明 on 15/11/24.
+//  Copyright © 2015年 郑克明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
