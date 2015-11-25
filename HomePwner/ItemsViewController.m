42016397ee966f8cc587dc96bae112c021411156
cocos543 <543314602@qq.com>
2015-11-25 19:19:35 +0800

    完成了 第8章 初级练习    增加一个表格段,上段现实大于某个值的数据,下段显示剩余的数据

--- HomePwner/ItemsViewController.m
+++ HomePwner/ItemsViewController.m
@@ -9,17 +9,34 @@
 #import "ItemsViewController.h"
 #import "ItemStore.h"
 #import "BNRItem.h"
-@implementation ItemsViewController
+@interface ItemsViewController()
+@property (nonatomic) NSInteger bigItemsCount;
+@property (nonatomic) NSArray *sortedItmes;
+@end
+@implementation ItemsViewController{
+    int _pBigItemsIndexs[100];
+}
 
 //无论调用哪一个初始化方法,最后都是返回一个UITableViewStylePlain类型的Table对象
 - (instancetype)init
 {
-    self = [super initWithStyle:UITableViewStylePlain];
+    self = [super initWithStyle:UITableViewStyleGrouped];
     if(self){
         for (int i =0; i<5; i++) {
             [[ItemStore sharedStore] createItem];
         }
     }
+    NSInteger val = 50;
+    _bigItemsCount =[self getBigItemsCount:val];
+    
+    //先初始化数组
+    //memset(_pBigItemsIndexs, -1, sizeof(_pBigItemsIndexs));
+    //[self getBigItemsIndexs:50 indexs:_pBigItemsIndexs];
+    
+    //获取排序之后的结果
+    _sortedItmes = [self getSortItemsBigThan:val formItems:[[ItemStore sharedStore] allItems]];
+    NSLog(@"%@",[[ItemStore sharedStore] allItems]);
+    
     return self;
 }
 
@@ -35,20 +52,79 @@
 // 注意,UITableViewController 本身就已经遵循了UITableViewDelegate, UITableViewDataSource 等协议
 // 详情可参考SDK
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return [[[ItemStore sharedStore] allItems] count];
+    
+    NSLog(@"%d",section);
+    int count =self.bigItemsCount;
+    if(section == 0){
+        return count;
+    }
+    return [[[ItemStore sharedStore] allItems] count] - count;
+}
+
+-(int)getBigItemsCount:(NSInteger)value{
+    static int count =0;
+    if(count >0){
+        return count;
+    }
+    NSArray *items = [[ItemStore sharedStore]allItems];
+    for (BNRItem *item in items) {
+        if(item.valueInDollars > value){
+            count++;
+        }
+    }
+    return count;
+}
+
+///未使用
+-(void)getBigItemsIndexs:(NSInteger)value indexs:(int *)indexs{
+    int key = 0;
+    NSArray *items = [[ItemStore sharedStore] allItems];
+    for (int i=0; i<items.count; i++) {
+        if ([items[i] valueInDollars] > value) {
+            indexs[key] = i;
+            key++;
+        }
+    }
+}
+
+-(NSArray *)getSortItemsBigThan:(NSInteger)value formItems:(NSArray *)items{
+    NSArray *sortedItems = [items sortedArrayUsingComparator:^(id obj1, id obj2) {
+        
+        if ([obj1 valueInDollars] < [obj2 valueInDollars]) {
+            return (NSComparisonResult)NSOrderedDescending;
+        }
+        
+        if ([obj1 valueInDollars] > [obj2 valueInDollars]) {
+            return (NSComparisonResult)NSOrderedAscending;
+        }
+        return (NSComparisonResult)NSOrderedSame;
+    }];
+    
+    return sortedItems;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"section = %ld",(long)indexPath.section);
+    NSLog(@"row = %ld",(long)indexPath.row);
     //UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
     //创建可重复使用的
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     
-    NSArray *items = [[ItemStore sharedStore]allItems];
-    BNRItem *item = items[indexPath.row];
+    NSArray *items = self.sortedItmes;
+    BNRItem *item;
+    if (indexPath.section == 0) {
+        item = items[indexPath.row];
+    }else{
+        item = items[indexPath.row + self.bigItemsCount];
+    }
     
     cell.textLabel.text = [item description];
     
     return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
 }
 
 -(void)viewDidLoad{
