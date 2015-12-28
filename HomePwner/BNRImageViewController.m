fed74822bd71b3d19a125bd10ffea4991e26111c
Cocos <543314602@qq.com>
2015-12-29 07:58:35 +0800

    完成第19章初级高级练习,双指缩放图片

--- HomePwner/BNRImageViewController.m
+++ HomePwner/BNRImageViewController.m
@@ -8,8 +8,10 @@
 
 #import "BNRImageViewController.h"
 
-@interface BNRImageViewController ()
+@interface BNRImageViewController () <UIScrollViewDelegate>
 
+@property (nonatomic,strong) UIImageView *imgView;
+@property (nonatomic,strong) UIScrollView *scrollView;
 @end
 
 @implementation BNRImageViewController
@@ -20,13 +22,27 @@
     UIImageView *imageView = [[UIImageView alloc] init];
     imageView.contentMode = UIViewContentModeScaleAspectFit;
     //这里不需要任何约束. UIImageView在被加进PopoverController之后会自动适配其长宽
-    self.view = imageView;
+    //self.view = imageView;
+    
+    CGRect screenRect = self.view.bounds;
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    scrollView.delegate = self;
+    scrollView.minimumZoomScale = 0.5;
+    scrollView.maximumZoomScale = 2;
+    [scrollView addSubview:imageView];
+    [scrollView setContentSize:CGSizeMake(600, 600)];
+    [scrollView setPagingEnabled:NO];
+    self.imgView = imageView;
+    self.scrollView = scrollView;
+    self.view = scrollView;
 }
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     //self.view 被定义未UIView,必须强制转换
-    UIImageView *imageView = (UIImageView *)self.view;
+    //UIImageView *imageView = (UIImageView *)self.view;
+    UIImageView *imageView = self.imgView;
+    imageView.frame = self.view.frame;
     imageView.image = self.image;
 }
 
@@ -44,5 +60,11 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+//缩放事件
+//缩放事件
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return self.imgView;
+}
 
 @end
